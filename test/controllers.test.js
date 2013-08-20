e492779bfcfca484d264ea2f2948bfee060b9f4e
Michael Dausmann <mdausmann@gmail.com>
2013-08-21 08:27:24 +1000

    complete  controller tests

--- test/controllers.test.js
+++ test/controllers.test.js
@@ -1,4 +1,5 @@
 describe( 'controller test', function() {
+  var scope;
   beforeEach( angular.mock.module( 'myApp' ) );
 
   //mock the controller for the same reason and include $rootScope and $controller
@@ -99,6 +100,159 @@ describe( 'controller test', function() {
     });
   });
 
+  it('should accept newUser and add it to the users list', function(){
+    var testUserInfo = {
+      "provider":'github',
+      "displayName":'bob',
+      "username":'bob the builder',
+      "profileUrl":'',
+      "emails":'bob@thebuilder.com',
+      "avatar_url": '',
+      "gravatar_id": ''
+    };
+
+    var newUser = {userId:'bob', isYou:false, userInfo:testUserInfo, role:'default'}
+    socketMock.emit('newUser', newUser);
+    expect(scope.users.length).toBe(1);
+    expect(scope.users[0]).toBe(newUser);
+
+  });
+
+  it('should accept newUser that is you and set current user and read only', function(){
+    socketMock.emit('roomReadOnly', false);
+
+    var testMap = {
+      moderator:{'editData':true, 'newChatMessage':true, 'changeUserId':true, 'saveCurrentFile': true, 'changeCurrentFile':true, 'changeRole':true},
+      editor:{'editData':true, 'newChatMessage':true, 'changeUserId':true, 'saveCurrentFile': true, 'changeCurrentFile':true, 'changeRole':false},
+      default:{'editData':true, 'newChatMessage':true, 'changeUserId':true, 'saveCurrentFile': false, 'changeCurrentFile':false, 'changeRole':false}
+    };
+
+    socketMock.emit('roomAuthMap', testMap);
+
+    var testUserInfo = {
+      "provider":'github',
+      "displayName":'bob',
+      "username":'bob the builder',
+      "profileUrl":'',
+      "emails":'bob@thebuilder.com',
+      "avatar_url": '',
+      "gravatar_id": ''
+    };
+
+    var newUser = {userId:'bob', isYou:true, userInfo:testUserInfo, role:'default'}
+    socketMock.emit('newUser', newUser);
+    expect(scope.currentUser).toBe(newUser);
+    expect(scope.readOnly).toBe(false);
+    expect(scope.editorOptions.readOnly).toBe(false);
+  });
+
+  it('should accept exitingUser and remove user from user list', function(){
+    socketMock.emit('newUser', {userId:'bob', isYou:true, userInfo:null, role:'default'});
+    socketMock.emit('newUser', {userId:'tara', isYou:true, userInfo:null, role:'default'});
+    socketMock.emit('newUser', {userId:'will', isYou:true, userInfo:null, role:'default'});
+    socketMock.emit('exitingUser', 'tara');
+    expect(scope.users.length).toBe(2);
+    expect(scope.users[1].userId).toBe('will');
+  });
+
+  it('should accept userIdChanged and change the users name', function(){
+    socketMock.emit('newUser', {userId:'bob', isYou:true, userInfo:null, role:'default'});
+    socketMock.emit('userIdChanged', 'bob', 'tara');
+    expect(scope.users[0].userId).toBe('tara');
+  });
+
+  it('should accept userRoleChanged and change the users name', function(){
+    socketMock.emit('roomReadOnly', false);
+
+    var testMap = {
+      moderator:{'editData':true, 'newChatMessage':true, 'changeUserId':true, 'saveCurrentFile': true, 'changeCurrentFile':true, 'changeRole':true},
+      editor:{'editData':true, 'newChatMessage':true, 'changeUserId':true, 'saveCurrentFile': true, 'changeCurrentFile':true, 'changeRole':false},
+      default:{'editData':false, 'newChatMessage':true, 'changeUserId':true, 'saveCurrentFile': false, 'changeCurrentFile':false, 'changeRole':false}
+    };
+
+    socketMock.emit('roomAuthMap', testMap);
+
+    socketMock.emit('newUser', {userId:'bob', isYou:true, userInfo:null, role:'default'});
+    socketMock.emit('userRoleChanged', 'bob', 'editor');
+    expect(scope.users[0].role).toBe('editor');
+    expect(scope.editorOptions.readOnly).toBe(false);
+  });
+
+  it('should accept requestedRole and change the users requested role', function(){
+    socketMock.emit('newUser', {userId:'bob', isYou:true, userInfo:null, role:'default'});
+    socketMock.emit('requestedRole', 'bob', 'editor');
+    expect(scope.users[0].requestedRole).toBe('editor');
+  });
+
+  it('should emit changeCurrentFile when requestChangeCurrentFile is called', function(){
+    scope.requestChangeCurrentFile('/test.js');
+    //emits['eventName'][#emit][#arg]
+    expect(socketMock.emits['changeCurrentFile'][0][0]).toBe('/test.js');
+  });
+
+  it('should emit saveCurrentFile when requestSaveCurrentFile is called', function(){
+    scope.requestSaveCurrentFile();
+    expect(socketMock.emits['saveCurrentFile'].length).toBe(1);
+  });
+
+  it('should emit newChatMessage and clear newMessage when newChatMessage is called', function(){
+    scope.newMessage = 'Hi my name is Earl.';
+    scope.newChatMessage();
+    expect(socketMock.emits['newChatMessage'][0][0]).toBe('Hi my name is Earl.');
+    expect(scope.newMessage).toBe('');
+  });
+
+  it('should emit changeUserId and clear newUserId when changeUserId is called', function(){
+    scope.newUserId = 'Earl';
+    scope.changeUserId();
+    expect(socketMock.emits['changeUserId'][0][0]).toBe('Earl');
+    expect(scope.newUserId).toBe('');
+  });
+
+  it('should emit grantChangeRole when grantChangeRole is called', function(){
+    scope.grantChangeRole('Earl', 'editor');
+    expect(socketMock.emits['grantChangeRole'].length).toBe(1);
+    expect(socketMock.emits['grantChangeRole'][0][0].userId).toBe('Earl');
+    expect(socketMock.emits['grantChangeRole'][0][0].newRole).toBe('editor');
+  });
+
+  it('should emit grantChangeRole if the user has a requested role and grantRequestedRole is called', function(){
+    var newUser = {userId:'bob', isYou:false, userInfo:null, role:'default'}
+    socketMock.emit('newUser', newUser);
+    socketMock.emit('requestedRole', 'bob', 'editor');
+
+    scope.grantRequestedRole('bob');
+
+    expect(socketMock.emits['grantChangeRole'].length).toBe(1);
+    expect(socketMock.emits['grantChangeRole'][0][0].userId).toBe('bob');
+    expect(socketMock.emits['grantChangeRole'][0][0].newRole).toBe('editor');
+  });
+
+  it('should emit requestChangeRole when requestChangeRole is called', function(){
+    var newUser = {userId:'bob', isYou:false, userInfo:null, role:'default'}
+    socketMock.emit('newUser', newUser);
+
+    scope.requestChangeRole('bob', 'editor', 'xxx123');
+
+    expect(socketMock.emits['requestChangeRole'].length).toBe(1);
+    expect(socketMock.emits['requestChangeRole'][0][0].userId).toBe('bob');
+    expect(socketMock.emits['requestChangeRole'][0][0].newRole).toBe('editor');
+    expect(socketMock.emits['requestChangeRole'][0][0].pass).toBe('xxx123');
+  });
+
+  it('should emit changeData and refreshData when onEditorChange is called and scope.readOnly is false', function(){
+    scope.readOnly = false;
+    scope.body = "this is the text";
+    var testOp = {op:'op'};
+
+    scope.onEditorChange(0, testOp);
+
+    expect(socketMock.emits['changeData'][0][0]).toBe(testOp);
+    expect(socketMock.emits['refreshData'][0][0]).toBe("this is the text");
+    expect(socketMock.emits['refreshData'][0][1]).toBe(false);
+  });
+
+
 
 });
 
@@ -109,6 +263,7 @@ thanks to https://github.com/southdesign for the idea
 */
 var sockMock = function($rootScope){
   this.events = {};
+  this.emits = {};
 
   // Receive Events
   this.on = function(eventName, callback){
@@ -119,6 +274,11 @@ var sockMock = function($rootScope){
   // Send Events
   this.emit = function(eventName){
     var args = Array.prototype.slice.call(arguments, 1);
+
+    if(!this.emits[eventName])
+      this.emits[eventName] = [];
+    this.emits[eventName].push(args);
+
     if(this.events[eventName]){
       angular.forEach(this.events[eventName], function(callback){
         $rootScope.$apply(function() {
